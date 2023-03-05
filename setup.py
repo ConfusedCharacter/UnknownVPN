a05681784cb3fc5ea3a338dab259598050bb6fea
Cᴏɴғᴜsᴇᴅ Cʜᴀʀᴀᴄᴛᴇʀ <121636230+ConfusedCharacter@users.noreply.github.com>
2023-03-05 02:43:38 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name = "UnknownVPN",
-    version = "1.0.0",
+    version = "1.0.2",
     description = "Simple Library for UnknownVPN Robot API documentation.",
     author = "Confused Character",
     packages = find_packages(),
